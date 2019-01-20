60cb28bae0ca50a05054971bf0ec59fb7151097e
ChauDinh <chaudvb95uc@gmail.com>
2019-01-21 02:56:18 +0700

    Create Add and Find Customer

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ const db = mongoose.connect("mongodb://localhost:27017/customer-cli", {
 const Customer = require("./Model/customer");
 
 // Add customer
-
+const addCustomer = customer => {
+ Customer.create(customer).then(customer => {
+  console.info("New Customer Added...");
+  db.close();
+ })
+}
 
 // Find customer
+const findCustomer = name => {
+ // Make case insensitive
+ const search = new RegExp(name, "i");
+ Customer.find({ $or: [{ firstname: search }, { lastname: search }] })
+  .then(customer => {
+   console.info(customer);
+   console.info(`${customer.length} matches`);
+   db.close();
+  })
+}
 
+module.exports = {
+ addCustomer,
+ findCustomer
+};
